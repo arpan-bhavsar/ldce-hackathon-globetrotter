baff57bd886457cf7e2af98b0935360ee838e759
Janvi-Soni214 <js2114oni@gmail.com>
2026-08-22 12:23:15 +0530

    created database table for social post

---
+++ backend/src/models/Post.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  authorName: { type: String, required: true },
+  location: { type: String, required: true },
+  content: { type: String, required: true },
+  likes: { type: Number, default: 0 }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Post', postSchema);
